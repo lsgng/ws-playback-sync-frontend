9877ad814e2594d0ffa1e57e8a2193a813792a91
Florian Liesegang <lysigk@gmail.com>
2021-02-23 11:24:23 +0100

    basic websocket communication

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as Tone from 'tone'
 import { Deck } from './Deck'
 import sample_01 from './assets/01.mp3'
 import sample_02 from './assets/02.mp3'
 
 export const App = () => {
+    const ws = useRef(null)
+    const [connected, setConnected] = useState(false)
+
+    useEffect(() => {
+        console.log('connecting')
+        ws.current = new WebSocket('ws://localhost:1234/ws')
+        ws.current.onopen = () => {
+            setConnected(true)
+        }
+        ws.current.onmessage = (msg) => {
+            console.log(msg)
+        }
+
+        return () => {
+            ws.current.close()
+        }
+    }, [])
+
     const [crossFade, setCrossFade] = useState(null)
     const [fade, setFade] = useState(50)
 
@@ -15,10 +33,13 @@ export const App = () => {
         }
     }
 
-    return (
+    return !connected ? (
+        <h1>Connecting to server...</h1>
+    ) : (
         <div>
             <button
                 onClick={() => {
+                    ws.current.send('AWASOBABA')
                     if (crossFade === null) {
                         initCrossFade()
                     }
