3bf8df9d9fef6103ba5f5227b8b311c42c9ea8fd
Florian Liesegang <lysigk@gmail.com>
2021-02-24 10:35:54 +0100

    sync play state via server

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,22 +10,72 @@ export const App = () => {
     const [connected, setConnected] = useState(false)
     const [registered, setRegistered] = useState(false)
 
+    const [player_A, setPlayer_A] = useState(null)
+    const [loaded_A, setLoaded_A] = useState(false)
+
+    const [lastStarted_MS_A, setLastStarted_MS_A] = useState(Date.now())
+    const [lastStartPoint_MS_B, setLastStartPoint_MS_B] = useState(0)
+
+    useEffect(() => {
+        if (player_A === null && crossFade !== null) {
+            const newPlayer = new Tone.Player()
+            newPlayer.connect(crossFade.a)
+            setPlayer_A(newPlayer)
+        }
+
+        if (player_A !== null && !loaded_A) {
+            player_A.load(sample_01).then(() => {
+                setLoaded_A(true)
+            })
+        }
+    })
+
+    useEffect(() => {
+        if (socket !== null) {
+            socket.onmessage = onMessage
+        }
+    })
+
+    const play = () => {
+        player_A.start()
+        setLastStarted_MS_A(Date.now())
+        setLastStartPoint_MS_B(0)
+    }
+
+    const forward = (offset_MS) => {
+        let now_MS = Date.now()
+        const timePlayed_MS = now_MS - lastStarted_MS_A + lastStartPoint_MS_B
+        const seekPosition_MS = timePlayed_MS + offset_MS
+        player_A.seek(seekPosition_MS / 1000, Tone.now())
+        setLastStarted_MS_A(now_MS)
+        setLastStartPoint_MS_B(seekPosition_MS)
+    }
+
+    const onMessage = (message) => {
+        const payload = JSON.parse(message.data).body
+        if (payload === 'register') {
+            setRegistered(true)
+        }
+        if (payload === 'play') {
+            play()
+        }
+        if (payload === 'stop') {
+            player_A.stop()
+        }
+    }
+
     useEffect(() => {
         if (socket === null) {
             const newSocket = new WebSocket('ws://localhost:1234/ws')
             newSocket.onopen = () => {
                 setConnected(true)
             }
-            newSocket.onmessage = (msg) => {
-                if (JSON.parse(msg.data).body === 'register') {
-                    setRegistered(true)
-                }
-            }
+            newSocket.onmessage = onMessage
             setSocket(newSocket)
         }
 
         return () => {
-            socket.current.close()
+            socket.close()
         }
     }, [])
 
@@ -64,8 +114,16 @@ export const App = () => {
             {crossFade !== null && registered && (
                 <div className="app_deck_container">
                     <div className="app_decks">
-                        <Deck crossFadeInput={crossFade.a} sample={sample_01} />
-                        <Deck crossFadeInput={crossFade.b} sample={sample_02} />
+                        <Deck
+                            onClickForward={forward}
+                            onClickPlay={() => {
+                                socket.send('play')
+                            }}
+                            onClickStop={() => {
+                                socket.send('stop')
+                            }}
+                        />
+                        <Deck />
                     </div>
                     <input
                         className="slider"
--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,78 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import * as Tone from 'tone'
 import './Deck.css'
 
-export const Deck = ({ crossFadeInput, sample }) => {
-    const [player, setPlayer] = useState(null)
-    const [loaded, setLoaded] = useState(false)
-
-    const [lastStarted_MS, setLastStarted_MS] = useState(Date.now())
-    const [lastStartPoint_MS, setLastStartPoint_MS] = useState(0)
-
-    useEffect(() => {
-        if (player === null) {
-            const newPlayer = new Tone.Player()
-            newPlayer.connect(crossFadeInput)
-            setPlayer(newPlayer)
-        }
-
-        if (player !== null && !loaded) {
-            player.load(sample).then(() => {
-                setLoaded(true)
-            })
-        }
-    })
-
-    const play = () => {
-        player.start()
-        setLastStarted_MS(Date.now())
-        setLastStartPoint_MS(0)
-    }
-
-    const forward = (offset_MS) => {
-        let now_MS = Date.now()
-        const timePlayed_MS = now_MS - lastStarted_MS + lastStartPoint_MS
-        const seekPosition_MS = timePlayed_MS + offset_MS
-        player.seek(seekPosition_MS / 1000, Tone.now())
-        setLastStarted_MS(now_MS)
-        setLastStartPoint_MS(seekPosition_MS)
-    }
-
+export const Deck = ({ onClickPlay, onClickStop, onClickForward }) => {
     return (
         <div className="deck">
             <div>
-                <button
-                    className="deck_button_play"
-                    disabled={!loaded}
-                    onClick={play}
-                >
+                <button className="deck_button_play" onClick={onClickPlay}>
                     Play
                 </button>
-                <button
-                    className="deck_button_stop"
-                    disabled={player == null}
-                    onClick={() => {
-                        player.stop()
-                    }}
-                >
+                <button className="deck_button_stop" onClick={onClickStop}>
                     Stop
                 </button>
             </div>
             <div>
                 <button
                     className="deck_button_forward"
-                    disabled={player === null}
                     onClick={() => {
-                        forward(25)
+                        onClickForward(25)
                     }}
                 >
                     +25 ms
                 </button>
                 <button
                     className="deck_button_forward"
-                    disabled={player === null}
                     onClick={() => {
-                        forward(100)
+                        onClickForward(100)
                     }}
                 >
                     +100 ms
